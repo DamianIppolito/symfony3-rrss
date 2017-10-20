fcdf21b631e67e92bd29f4b7de0cccc4f5086627
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-10-20 14:00:18 +0200

    Seccion 4 - 13 - Probar una entidad (Modelo y Repositorio)

--- src/BackendBundle/Controller/DefaultController.php
+++ src/BackendBundle/Controller/DefaultController.php
@@ -8,6 +8,12 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getEntityManager();
+        $user_repo = $em->getRepository("BackendBundle:User");
+        $user = $user_repo->find(1);
+        echo "Bienvenido: ".$user->getName()." ".$user->getSurname();
+        var_dump($user);
+        die();
         return $this->render('BackendBundle:Default:index.html.twig');
     }
 }
