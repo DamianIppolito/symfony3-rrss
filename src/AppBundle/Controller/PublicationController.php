d618f5b2c011d0da03d32ebfcdc9c0ffcfa970d5
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-10-25 13:34:20 +0200

    Seccion - 10 - 46 - Crear formulario de publicaciones

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -8,13 +8,20 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\PublicationType;
+use BackendBundle\Entity\Publication;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PublicationController extends Controller
 {
     public function indexAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $publication = new Publication();
+        $form = $this->createForm(PublicationType::class,$publication);
+
         return $this->render('AppBundle:Publication:home.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 }
