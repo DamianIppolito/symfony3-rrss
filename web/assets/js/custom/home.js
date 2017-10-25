b6acd9bd9b4f17e1f6c968ac2b7b78376ac3876f
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-10-25 17:36:37 +0200

    Seccion 10 - 53 - Scroll infinito en la Timeline

---
+++ web/assets/js/custom/home.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    var ias = jQuery.ias({
+        container: '#timeline .box-content',
+        item: '.publication-item',
+        pagination: '.pagination',
+        next: '#timeline .pagination .next_link',
+        triggerpageThreshold: 1
+    });
+
+    ias.extension(new IASTriggerExtension({
+        text: 'Ver más publicaciones',
+        offset: 2
+    }));
+
+    ias.extension(new IASSpinnerExtension({
+        src: URL + '/../assets/images/ajax-loader.gif'
+    }));
+
+    ias.extension(new IASNoneLeftExtension({
+        text: 'No hay más publicaciones'
+    }));
+
+    ias.on('ready',function (event) {
+        buttons();
+    });
+
+    ias.on('rendered',function (event) {
+        buttons();
+    });
+});
+
+function buttons() {
+
+}
