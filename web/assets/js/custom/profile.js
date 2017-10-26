322e321b86aa1305ba080a312e913777a5902d07
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-10-26 17:40:39 +0200

    Seccion 12 - 65 - Enlaces y JavaScript de los perfiles

---
+++ web/assets/js/custom/profile.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    var ias = jQuery.ias({
+        container: '.profile-box #user-publications',
+        item: '.publication-item',
+        pagination: '.profile-box #user-publications .pagination',
+        next: '.profile-box #user-publications .pagination .next_link',
+        triggerpageThreshold: 1
+    });
+
+    ias.extension(new IASTriggerExtension({
+        text: 'Ver más publicaciones',
+        offset: 2
+    }));
+
+    ias.extension(new IASSpinnerExtension({
+        src: URL + '/../assets/images/ajax-loader.gif'
+    }));
+
+    ias.extension(new IASNoneLeftExtension({
+        text: 'No hay más publicaciones'
+    }));
+
+    ias.on('ready',function (event) {
+        buttons();
+    });
+
+    ias.on('rendered',function (event) {
+        buttons();
+    });
+});
+
+function buttons() {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('.btn-image').unbind().click(function(){
+        $(this).parent().find('.pub-image').fadeToggle();
+    });
+
+    $('.btn-delete-pub').unbind().click(function(){
+        $(this).parent().parent().addClass('hidden');
+        $.ajax({
+            url: URL + '/publication/remove/' + $(this).attr('data-id'),
+            type: 'GET',
+            success: function (response) {
+                console.log(response);
+            }
+        });
+    });
+
+    $('.btn-like').unbind().click(function(){
+        $(this).addClass('hidden');
+        $(this).parent().find('.btn-unlike').removeClass('hidden');
+        $.ajax({
+            url: URL + '/like/' + $(this).attr('data-id'),
+            type: 'GET',
+            success: function (response) {
+                console.log(response);
+            }
+        });
+    });
+
+    $('.btn-unlike').unbind().click(function(){
+        $(this).addClass('hidden');
+        $(this).parent().find('.btn-like').removeClass('hidden');
+        $.ajax({
+            url: URL + '/unlike/' + $(this).attr('data-id'),
+            type: 'GET',
+            success: function (response) {
+                console.log(response);
+            }
+        });
+    });
+}
