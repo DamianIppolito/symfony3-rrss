ee1088c3de1d115a468a223200ff968453284b63
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-02 17:59:38 +0100

    Seccion 14 - 79 - Enviar mensaje privado

--- src/AppBundle/Controller/PrivateMessageController.php
+++ src/AppBundle/Controller/PrivateMessageController.php
@@ -29,10 +29,61 @@ class PrivateMessageController extends Controller{
         $user = $this->getUser();
         $private_message = new PrivateMessage();
         $form = $this->createForm(PrivateMessageType::class, $private_message, array('empty_data' => $user));
+        $form->handleRequest($request);
+        $message_class='alert-danger';
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                //upload image
+                $file = $form['image']->getData();
+                if(!empty($file) && $file != null){
+                    $ext = $file->guessExtension();
+                    if($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif'){
+                        $file_name = $user->getId().time().'.'.$ext;
+                        $file->move('uploads/messages/images',$file_name);
+                        $private_message->setImage($file_name);
+                    }else{
+                        $private_message->setImage(null);
+                    }
+                }else{
+                    $private_message->setImage(null);
+                }
+                //upload document
+                $document = $form['file']->getData();
+                if(!empty($document) && $document != null){
+                    $ext = $document->guessExtension();
+                    if($ext == 'pdf'){
+                        $file_name = $user->getId().time().'.'.$ext;
+                        $document->move('uploads/message/documents',$file_name);
+                        $private_message->setFile($file_name);
+                    }else{
+                        $private_message->setFile(null);
+                    }
+                }else{
+                    $private_message->setFile(null);
+                }
+                $private_message->setEmitter($user);
+                $private_message->setCreatedAt(new \DateTime('now'));
+                $private_message->setReaded(0);
+
+                $em->persist($private_message);
+                $flush = $em->flush();
+                if($flush == null){
+                    $status = 'El mensaje privado se ha enviado correctamente';
+                    $message_class='alert-success';
+                }else{
+                    $status = 'El mensaje privado no se ha enviado';
+                }
+            }else{
+                $status = 'El mensaje privado no se ha enviado';
+            }
+            $this->session->getFlashBag()->add('status', $status);
+            $this->session->getFlashBag()->add("message_class",$message_class);
+            return $this->redirectToRoute('private_message_index');
+        }
 
         return $this->render('@App/PrivateMessage/index.html.twig',array(
             'form' => $form->createView(),
-            'titulo' => 'Titulo'
+
         ));
     }
 }
