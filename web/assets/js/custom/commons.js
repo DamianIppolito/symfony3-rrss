1ca85bafef095a20a1f27d503b26211967f1ee2e
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-02 14:02:03 +0100

    Seccion 13 - 74 - Mostrar las notificaciones sin leer con Ajax

---
+++ web/assets/js/custom/commons.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    notifications();
+    setInterval(function () {notifications()},60000);
+});
+
+function notifications() {
+    $.ajax({
+        url: URL + '/notifications/get',
+        type: 'GET',
+        success: function (response) {
+            $('.label-notifications').html(response);
+            show_or_hide_notifications();
+        }
+    });
+}
+
+function show_or_hide_notifications() {
+    if($('.label-notifications').text == 0){
+        $('.label-notifications').addClass('hidden');
+    }else{
+        $('.label-notifications').removeClass('hidden');
+    }
+}
