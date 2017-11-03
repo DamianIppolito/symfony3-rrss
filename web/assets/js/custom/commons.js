08bb7cbcf13cb75c41ff7ed6048e10d68d6fdc6b
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-03 12:24:35 +0100

    Seccion 14 - 82 - Notificaciones de MD

--- web/assets/js/custom/commons.js
+++ web/assets/js/custom/commons.js
@@ -1,23 +1,40 @@
 $(document).ready(function () {
-    notifications();
-    setInterval(function () {notifications()},60000);
+    new_alerts();
+    setInterval(function () {new_alerts()},60000);
 });
 
-function notifications() {
+function new_alerts() {
     $.ajax({
         url: URL + '/notifications/get',
         type: 'GET',
         success: function (response) {
             $('.label-notifications').html(response);
-            show_or_hide_notifications();
+            show_or_hide_new_alerts('notifications');
+        }
+    });
+
+    $.ajax({
+        url: URL + '/private-message/notification/get',
+        type: 'GET',
+        success: function (response) {
+            $('.label-private-messages').html(response);
+            show_or_hide_new_alerts('private-messages');
         }
     });
 }
 
-function show_or_hide_notifications() {
-    if($('.label-notifications').text() == 0){
-        $('.label-notifications').addClass('hidden');
+function show_or_hide_new_alerts(type) {
+    if(type == 'notifications'){
+        if($('.label-notifications').text() == 0){
+            $('.label-notifications').addClass('hidden');
+        }else{
+            $('.label-notifications').removeClass('hidden');
+        }
     }else{
-        $('.label-notifications').removeClass('hidden');
+        if($('.label-private-messages').text() == 0){
+            $('.label-private-messages').addClass('hidden');
+        }else{
+            $('.label-private-messages').removeClass('hidden');
+        }
     }
 }
